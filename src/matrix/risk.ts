c4e2f4dd15cf3ec1ec4ae8cb3b58bf4f05a25097
David Ferguson <David.Ferguson@oak.com>
2020-01-10 21:39:10 +0000

    Updated matrix to fix issues with the Risk section

--- src/matrix/risk.ts
+++ src/matrix/risk.ts
@@ -20,9 +20,9 @@ export default class Risk extends MatrixBase {
         this.matrix["Certain"]["Moderate"] = "Protect Position, invest to fix immediately everything else goes on hold";
         this.matrix["Certain"]["Catastrophic"] = "Protect Position, invest to fix immediately everything else goes on hold";
 
-        this.scoreLookup["Accept risk"] = 9;
-        this.scoreLookup["Create mitigating strategies, monitor risk regularly"] = 6;
-        this.scoreLookup["Protect position, invest to fix ASAP, gets top priority"] = 2;
-        this.scoreLookup["Protect Position, invest to fix immediately everything else goes on hold"] = 1;
+        this.scoreLookup["Accept risk"] = 1;
+        this.scoreLookup["Create mitigating strategies, monitor risk regularly"] = 2;
+        this.scoreLookup["Protect position, invest to fix ASAP, gets top priority"] = 6;
+        this.scoreLookup["Protect Position, invest to fix immediately everything else goes on hold"] = 9;
     }
 }
